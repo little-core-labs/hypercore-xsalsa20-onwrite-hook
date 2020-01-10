703d7af794eb83853756a24249eb3177b0b39b76
Joseph Werle <joseph.werle@gmail.com>
2020-01-09 19:53:02 -0500

    refactor(): Use 'hypercore-indexed-file' in example

--- example.js
+++ example.js
@@ -1,6 +1,7 @@
 const replicate = require('hypercore-replicate')
 const hypercore = require('hypercore')
 const crypto = require('hypercore-crypto')
+const File = require('hypercore-indexed-file')
 const pump = require('pump')
 const hook = require('./')
 const ram = require('random-access-memory')
@@ -10,36 +11,39 @@ const key = crypto.randomBytes(32)
 const { publicKey, secretKey } = crypto.keyPair()
 const nonces = ram()
 
-const source = hypercore(ram, publicKey, {
+const source = File(__filename, {
+  key: publicKey,
   secretKey,
-})
+}, (err) => {
 
-const cipher = hypercore(ram, publicKey, {
-  secretKey,
-  onwrite: hook(nonces, key)
-})
+  const cipher = hypercore(ram, publicKey, {
+    secretKey,
+    onwrite: hook(nonces, key)
+  })
 
-const edge = hypercore(ram, publicKey)
+  const edge = hypercore(ram, publicKey)
 
-const reader = hypercore(ram, publicKey, {
-  onwrite: hook(nonces, key)
-})
+  const reader = hypercore(ram, publicKey, {
+    onwrite: hook(nonces, key)
+  })
 
-source.append(Buffer.from('hello'), (err) => {
-  source.head(console.log) // plaintext
+  source.ready(() => {
+    source.head(console.log) // plaintext
 
-  // load cipher hypercore
-  pump(source.createReadStream(), cipher.createWriteStream(), (err) => {
-    if (err) throw err
-    cipher.head(console.log) // ciphertext
-
-    replicate(cipher, edge, (err) => {
+    // load cipher hypercore
+    pump(source.createReadStream(), cipher.createWriteStream(), (err) => {
       if (err) throw err
-      edge.head(console.log) // ciphertext
+      cipher.head(console.log) // ciphertext
 
-      replicate(edge, reader, (err) => {
+      replicate(cipher, edge, (err) => {
         if (err) throw err
-        reader.head(console.log) // plaintext
+        edge.head(console.log) // ciphertext
+
+        replicate(edge, reader, (err) => {
+          if (err) throw err
+          reader.head(console.log) // plaintext
+          reader.createReadStream().pipe(process.stdout)
+        })
       })
     })
   })
